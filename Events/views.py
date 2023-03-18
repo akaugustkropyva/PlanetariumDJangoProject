dcc73a204c2116209700ea9801adeb656d9f3c19
JuliManhupli <juliamangup@gmail.com>
2023-03-18 17:43:27 +0200

    made all events and one event templates, added block title

--- Events/views.py
+++ Events/views.py
@@ -1,16 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Event
 
-
-# Create your views here.
 
 def filters(request, date: str, halls: str, sorting: str):
     return HttpResponse(f"<h1>All events filtered by {date}, {halls}, {sorting}<h1>")
 
 
 def all_events(request):
-    return HttpResponse("<h1>All events<h1>")
+    events = Event.objects.all()
+    return render(request, "all_events.html", context={'events': events})
 
 
-def event(request, parameter: int):
-    return HttpResponse(f"<h1>Event {parameter}<h1>")
+def event(request, parameter):
+    event = Event.objects.get(id=parameter)
+    return render(request, "event.html", context={'event': event})

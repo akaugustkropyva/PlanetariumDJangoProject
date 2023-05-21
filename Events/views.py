e655c271856a6b27d73b2db40696e4d9f67ac08c
Anne Kotova <82156094+akaugust@users.noreply.github.com>
2023-05-21 18:08:53 +0300

    Sorting + Search done, cart html almost done

--- Events/views.py
+++ Events/views.py
@@ -9,9 +9,21 @@ def filters(request, date: str, halls: str, sorting: str):
 
 def all_events(request):
     events = Event.objects.all()
+    sorting = request.GET.get('sorting', '')
+    search_query = request.GET.get('search', '')
+    date_picker = request.GET.get('date', '')
+    # print(date_picker)
+    if search_query is not None and search_query != ' ':
+        events = events.filter(name__icontains=search_query)
+    if sorting:
+        events = events.order_by(sorting)
+    if date_picker:
+        print(events.get('from_date'))
+        events = events.filter(date_picker__range=('from_date', 'to_date'))
+
     return render(request, "all_events.html", context={'events': events})
 
 
 def event(request, parameter):
-    event = Event.objects.get(id=parameter)
-    return render(request, "event.html", context={'event': event})
+    eventname = Event.objects.get(id=parameter)
+    return render(request, "event.html", context={'event': eventname})

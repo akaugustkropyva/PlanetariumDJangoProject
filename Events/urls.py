cce7e5be1cf976d25bc892193fc6c21689d75954
JuliManhupli <juliamangup@gmail.com>
2023-05-24 21:51:17 +0300

    favorite is done

--- Events/urls.py
+++ Events/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, re_path
-from .views import all_events, event, filters
+from .views import all_events, event, favourite_event
 
 app_name = 'events'
 urlpatterns = [
     path('', all_events, name='all_events'),
     path('<int:parameter>/', event, name='event'),
-    re_path(r'^(?P<date>(2023-(0[1-9]|1[0-2])-0[1-9]|[1-2][0-9]|3[0-1]))/(?P<halls>(big|small|stary))/'
-            r'(?P<sorting>(popularity|price))/$', filters)
+    path('<int:parameter>/favourite_event', favourite_event, name='favourite_event'),
 ]
 
